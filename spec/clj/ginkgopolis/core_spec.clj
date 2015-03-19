dd018a46e1b228cb374cde7accc809b28d852aaa
Arnauld <arnauld.loyer@gmail.com>
2015-03-19 16:34:22 +0100

    reorganize core namespace: actions/cards + start setup

--- spec/clj/ginkgopolis/core_spec.clj
+++ spec/clj/ginkgopolis/core_spec.clj
@@ -24,6 +24,17 @@
        (map :letter)
        (sort)))
 
+(defn- card-with-number [cards number]
+  (some #(= number (:number %)) cards))
+
+(describe "cartesian"
+          (it "should return the cartesian product of two lists"
+              (should= (for [x [-1 0 1]
+                             y [-1 0 1]] (vector x y))
+                       [[-1 -1] [-1 0] [-1 1]
+                        [0 -1] [0 0] [0 1]
+                        [+1 -1] [+1 0] [+1 1]])))
+
 (describe "The Building Tiles"
           (it "should contain 60 tiles"
               (should= 60 (count (building-tiles))))
@@ -45,5 +56,40 @@
           (it "should be lettered from 'A' to 'L'"
               (should= '(:A :B :C :D :E :F :G :H :I :J :K :L)
                        (letter-for-type (urbanization-cards) :urbanization))))
+
+(describe "The Building cards"
+          (xit "should contain 60 cards"
+               (should= 60 (count (building-cards))))
+          (it "should all be typed 'building'"
+              (should= (count (building-cards)) (count-for-type (building-cards) :building)))
+          (it "should contain 20 red cards"
+              (should= 20 (count-for-color (building-cards) :red))))
+
+(describe "Building card action - red ones"
+          (xit "should for card #1: gain a resource on exploit action"
+               (let [card (card-with-number (building-cards) 1)
+                     action (new-action card)]
+                 (should-not-be-nil card))))
+
+
+(describe "Game setup"
+          (it "should place the 9 building tiles numbered from 1, 2 and 3 for each color"
+              (let [setup (setup)
+                    setup-tiles-by-coord (:tiles setup)
+                    setup-tiles (vals setup-tiles-by-coord)]
+                (should= 9 (count setup-tiles-by-coord))
+                (should= [1 2 3] (numbers-for-color setup-tiles :blue))
+                (should= [1 2 3] (numbers-for-color setup-tiles :yellow))
+                (should= [1 2 3] (numbers-for-color setup-tiles :red))))
+          (it "should place the 9 building tiles in a 3x3 grid"
+              (let [setup (setup)
+                    setup-tiles-by-coord (:tiles setup)
+                    coords (keys setup-tiles-by-coord)
+                    grid (for [x [-1 0 1] y [-1 0 1]] (vector x y))]
+                (should= (sort coords) (sort grid))))
+          (it "should place *randomly* the 9 building tiles"
+              (let [setup1 (setup)
+                    setup2 (setup)]
+                (should-not= (:tiles setup1) (:tiles setup2)))))
 
 (run-specs)
--- src/clj/ginkgopolis/core.clj
+++ src/clj/ginkgopolis/core.clj
@@ -1,5 +1,65 @@
 (ns ginkgopolis.core)
 
+;         _   _ _
+;   _   _| |_(_) |___
+;  | | | | __| | / __|
+;  | |_| | |_| | \__ \
+;   \__,_|\__|_|_|___/
+
+
+;     _        _   _
+;    /_\   ___| |_(_) ___  _ __  ___
+;   //_\\ / __| __| |/ _ \| '_ \/ __|
+;  /  _  \ (__| |_| | (_) | | | \__ \
+;  \_/ \_/\___|\__|_|\___/|_| |_|___/
+
+(defn- on-action [actionType funcs]
+  {:when   :on-action
+   :filter (fn [action _game]
+             (= (:action-type action) actionType))
+   :apply  (fn [action game]
+             (reduce (fn [game0 func] (func action game0)) game funcs))})
+
+(defn- on-endgame [func]
+  {:when  :on-endgame
+   :apply func})
+
+(defn- resource-gain [_action game]
+  (throw (UnsupportedOperationException. "Not implemented")))
+
+(defn- point-gain [_action game]
+  (throw (UnsupportedOperationException. "Not implemented")))
+
+
+(defn- tile-color-bonus [color]
+  (fn [& args] (throw (UnsupportedOperationException. "Not implemented"))))
+
+(defn- tile-height-bonus [filter bonus]
+  (fn [& args] (throw (UnsupportedOperationException. "Not implemented"))))
+
+(defn- card-type-bonus [cardType bonus]
+  (fn [& args] (throw (UnsupportedOperationException. "Not implemented"))))
+
+(defn- card-color-bonus [cardColor bonus]
+  (fn [& args] (throw (UnsupportedOperationException. "Not implemented"))))
+
+(defn- point-bonus [bonus]
+  (fn [& args] (throw (UnsupportedOperationException. "Not implemented"))))
+
+(defn new-action
+  ([card]
+   (new-action card {}))
+  ([card params]
+   {:what   :action
+    :card   card
+    :params params}))
+
+;    ___              _
+;   / __\__ _ _ __ __| |___
+;  / /  / _` | '__/ _` / __|
+; / /__| (_| | | | (_| \__ \
+; \____/\__,_|_|  \__,_|___/
+
 (defn building-tiles []
   (let [numbers (range 1 21)]
     (reduce (fn [acc num]
@@ -11,16 +71,77 @@
 
 (defn urbanization-cards []
   (map #(assoc % :card-type :urbanization)
-       '(
-          {:letter :A}
-          {:letter :B}
-          {:letter :C}
-          {:letter :D}
-          {:letter :E}
-          {:letter :F}
-          {:letter :G}
-          {:letter :H}
-          {:letter :I}
-          {:letter :J}
-          {:letter :K}
-          {:letter :L})))
+       [
+        {:letter :A}
+        {:letter :B}
+        {:letter :C}
+        {:letter :D}
+        {:letter :E}
+        {:letter :F}
+        {:letter :G}
+        {:letter :H}
+        {:letter :I}
+        {:letter :J}
+        {:letter :K}
+        {:letter :L}]))
+
+(defn- new-building-card [color number actions]
+  (-> {}
+      (assoc :card-type :building)
+      (assoc :color color)
+      (assoc :number number)
+      (into actions)))
+
+(defn building-cards-blue []
+  [])
+
+(defn building-cards-yellow []
+  [])
+
+(defn building-cards-red []
+  [
+   (new-building-card :red 1 (on-action :exploit [resource-gain]))
+   (new-building-card :red 2 (on-action :urbanization [resource-gain]))
+   (new-building-card :red 3 (on-action :floor-construction [resource-gain]))
+   (new-building-card :red 4 (on-action :exploit [resource-gain]))
+   (new-building-card :red 5 (on-action :urbanization [resource-gain]))
+   (new-building-card :red 6 (on-action :floor-construction [resource-gain]))
+   (new-building-card :red 7 (on-action :exploit [resource-gain point-gain]))
+   (new-building-card :red 8 (on-action :urbanization [resource-gain point-gain]))
+   (new-building-card :red 9 (on-action :floor-construction [resource-gain point-gain]))
+   (new-building-card :red 10 (on-endgame (tile-color-bonus :red)))
+   (new-building-card :red 11 (on-endgame (tile-color-bonus :yellow)))
+   (new-building-card :red 12 (on-endgame (tile-color-bonus :blue)))
+   (new-building-card :red 13 (on-endgame (tile-color-bonus :red)))
+   (new-building-card :red 14 (on-endgame (tile-height-bonus #(or (= 1 %) (= 2 %)) 1)))
+   (new-building-card :red 15 (on-endgame (tile-height-bonus #(<= 3 %) 3)))
+   (new-building-card :red 16 (on-endgame (card-type-bonus :exploit 2)))
+   (new-building-card :red 17 (on-endgame (card-type-bonus :urbanization 2)))
+   (new-building-card :red 18 (on-endgame (card-type-bonus :floor-construction 2)))
+   (new-building-card :red 19 (on-endgame (card-color-bonus :red 2)))
+   (new-building-card :red 20 (on-endgame (point-bonus 9)))
+   ])
+
+(defn building-cards []
+  (-> []
+      (into (building-cards-blue))
+      (into (building-cards-yellow))
+      (into (building-cards-red))
+      ))
+
+;     ___
+;    / _ \__ _ _ __ ___   ___
+;   / /_\/ _` | '_ ` _ \ / _ \
+;  / /_\\ (_| | | | | | |  __/
+;  \____/\__,_|_| |_| |_|\___|
+
+(defn setup []
+  (let [tiles (group-by (fn [tile]
+                          (if (<= (:number tile) 3)
+                            :setup
+                            :remaining)) (building-tiles))]
+    {:tiles (zipmap (shuffle [[-1 1] [0 1] [1 1]
+                              [-1 0] [0 0] [1 0]
+                              [-1 -1] [0 -1] [1 -1]]) (:setup tiles))}))
+
+
